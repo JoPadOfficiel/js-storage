93c3dc377a03902dbcc573f33eecefaf58c836e5
JoPadOfficiel <100805221+JoPadOfficiel@users.noreply.github.com>
2025-01-13 15:22:29 +0100

    update

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const contacts = JSON.parse(localStorage.getItem('contacts')) || [];
 
-// Afficher les contacts
 function afficherContacts() {
     const liste = document.getElementById('listeContacts');
-    liste.innerHTML = contacts.map((c, i) => 
-        `- ${i+1}. Nom : ${c.nom}, Email : ${c.email}, Téléphone : ${c.telephone}`
+    liste.innerHTML = contacts.map((c) => 
+        `- Nom : ${c.nom}, Email : ${c.email}, Téléphone : ${c.telephone}`
     ).join('<br>');
 }
+
 
 // Ajouter un contact
 document.getElementById('contactForm').addEventListener('submit', e => {
@@ -18,10 +18,54 @@ document.getElementById('contactForm').addEventListener('submit', e => {
         telephone: document.getElementById('telephone').value
     });
     
-    // Sauvegarder et actualiser
+    // Enregistrer et actualiser
     localStorage.setItem('contacts', JSON.stringify(contacts));
     afficherContacts();
     e.target.reset();
 });
 
+
 afficherContacts();
+
+
+
+
+
+
+/*
+
+// Version 2 with tableau
+
+// Ajouter un contact
+document.getElementById('contactForm').addEventListener('submit', e => {
+    e.preventDefault();
+    
+    contacts.push({
+        nom: document.getElementById('nom').value,
+        email: document.getElementById('email').value,
+        telephone: document.getElementById('telephone').value
+    });
+    
+
+    // Enregistrer et actualiser
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+    afficherContacts();
+    e.target.reset();
+    //console.log(contacts);
+    for (let i = 0; i < contacts.length; i++) {    
+        console.log(contacts[i]);
+        localStorage.setItem('contacts', JSON.stringify(contacts))[i];
+        afficherContacts();
+        //console.log(contacts[i].nom);
+        //console.log(contacts[i].email);
+        //console.log(contacts[i].telephone);
+    }
+    function afficherContacts() {
+        const liste = document.getElementById('listeContacts');
+        liste.innerHTML = contacts.map((c) => 
+            `- Nom : ${c.nom}, Email : ${c.email}, Téléphone : ${c.telephone}`
+        ).join('<br>');
+    }
+    afficherContacts();
+});
+*/

7bf92c269609860aed5034e9f60dead55f8aa858
JoPadOfficiel <100805221+JoPadOfficiel@users.noreply.github.com>
2025-01-13 16:50:02 +0100

    Update - Exo 2

--- README.md
+++ README.md
@@ -16,3 +16,24 @@ Liste des contacts :
     - Nom : Naymard, Email : jean.naymard@gmail.com, Téléphone : 01 47 46 45 44
     - Nom : Zola, Email : emile-zola@hotmail.com, Téléphone : 06 77 76 75 74
 ```
+
+---
+## Exo 2
+
+Créer une page où l'utilisateur peut enregistrer plusieurs articles dans un panier d'achat avec localStorage et afficher le total des articles.
+
+#### Résultat attendu
+
+Titre : Ajouter un produit au panier
+```
+Formulaire
+    - Produit : <input>
+    - Prix : <input>
+
+Contenu du panier :
+
+    - Lait : 12 €
+    - Sucre : 5 €
+
+Total : 17 €
+```
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// *** Exo 1 ***
+/*
 const contacts = JSON.parse(localStorage.getItem('contacts')) || [];
 
 function afficherContacts() {
@@ -26,46 +28,40 @@ document.getElementById('contactForm').addEventListener('submit', e => {
 
 
 afficherContacts();
+*/
 
+// *** Exo 2 **
 
+const produits = JSON.parse(localStorage.getItem('produits')) || [];
 
+function afficherProduits() {
+    const liste = document.getElementById('listeProduits');
+    liste.innerHTML = produits.map((p) => 
+        `- ${p.produit} : ${p.prix}, €`
+    ).join('<br>');
+    // Calculer le total
+    const total = produits.reduce((sum, p) => sum + parseFloat(p.prix), 0);
+    
+    /*
+    let total = 0;
+    for(let i = 0; i < produits.length; i++) {
+        total += +produits[i].prix;
+    }
+    */
+    liste.innerHTML += `<br><br>Total : ${total.toFixed(2)} €`;
+}
 
-
-
-/*
-
-// Version 2 with tableau
-
-// Ajouter un contact
-document.getElementById('contactForm').addEventListener('submit', e => {
+// Ajouter un produit
+document.getElementById('produitForm').addEventListener('submit', e => {
     e.preventDefault();
     
-    contacts.push({
-        nom: document.getElementById('nom').value,
-        email: document.getElementById('email').value,
-        telephone: document.getElementById('telephone').value
+    produits.push({
+        produit: document.getElementById('produit').value,
+        prix: document.getElementById('prix').value,
     });
     
-
     // Enregistrer et actualiser
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-    afficherContacts();
+    localStorage.setItem('produits', JSON.stringify(produits));
+    afficherProduits();
     e.target.reset();
-    //console.log(contacts);
-    for (let i = 0; i < contacts.length; i++) {    
-        console.log(contacts[i]);
-        localStorage.setItem('contacts', JSON.stringify(contacts))[i];
-        afficherContacts();
-        //console.log(contacts[i].nom);
-        //console.log(contacts[i].email);
-        //console.log(contacts[i].telephone);
-    }
-    function afficherContacts() {
-        const liste = document.getElementById('listeContacts');
-        liste.innerHTML = contacts.map((c) => 
-            `- Nom : ${c.nom}, Email : ${c.email}, Téléphone : ${c.telephone}`
-        ).join('<br>');
-    }
-    afficherContacts();
 });
-*/
